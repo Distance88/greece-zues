5fb367cd36ac6b6dd8aacc1634da52dd90bd4600
Distance88 <49811451+Distance88@users.noreply.github.com>
2022-02-22 14:45:08 +0800

    Update OssUtils.java

--- greece-hades/src/main/java/com/zhang/project/biz/util/OssUtils.java
+++ greece-hades/src/main/java/com/zhang/project/biz/util/OssUtils.java
@@ -10,14 +10,5 @@ import com.aliyun.oss.OSSClientBuilder;
  */
 public class OssUtils {
 
-    private static String accessKeyId = "LTAI4G9mTsbTkpMhcjHbSeaF";
-
-    private static String accessKeySecret = "WGA0yVmPO36uqLxTICbg1CcvJVwmL9";
-
-    private static String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
-
-
-    public static OSS getOssClient(){
-        return new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
-    }
+   
 }
